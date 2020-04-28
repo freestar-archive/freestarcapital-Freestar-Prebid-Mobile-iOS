1889db10c02ccd2023ef8ba7443814d9348e6566
Dean Chang <dean.chang@freestar.io>
2020-04-28 15:15:07 -0700

    Fix module-map typo and umbrella header.

---
+++ sdk/PrebidMobile/PrebidMobileFS-umbrella.h
@@ -0,0 +1,38 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "PBServerAdapter.h"
+#import "PBAdUnit.h"
+#import "PBBannerAdUnit.h"
+#import "PBBidManager.h"
+#import "PBBidResponse.h"
+#import "PBBidResponseDelegate.h"
+#import "PBConstants.h"
+#import "PBException.h"
+#import "PBGlobalFunctions.h"
+#import "PBHost.h"
+#import "PBInterstitialAdUnit.h"
+#import "PBKeywordsManager.h"
+#import "PBTargetingParams.h"
+#import "PrebidCache.h"
+#import "PrebidMobile.h"
+#import "PBLogging.h"
+#import "PBLogManager.h"
+#import "PBAnalyticsEvent.h"
+#import "PBAnalyticsManager.h"
+#import "PBAnalyticsNSURLProtocol.h"
+#import "PBAnalyticsService.h"
+#import "PBCanonicalRequest.h"
+#import "PBNSURLSessionDemux.h"
+
+FOUNDATION_EXPORT double PrebidMobileFSVersionNumber;
+FOUNDATION_EXPORT const unsigned char PrebidMobileFSVersionString[];
