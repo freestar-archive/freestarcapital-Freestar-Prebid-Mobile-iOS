be057129ad57ea6a659e79149ded2f29ed055f38
Dean Chang <dean.chang@freestar.io>
2020-04-28 23:06:43 -0700

    Update project file, build script, and removed CMP tool.

--- scripts/buildPrebidMobile.sh
+++ scripts/buildPrebidMobile.sh
@@ -1,56 +1,38 @@
-# Merge Script
-if [ -d "scripts" ]; then
-cd scripts/
+#!/bin/bash
+
+PROJECT_NAME="PrebidMobileFS"
+PROJECT_DIR="../sdk"
+WORKSPACE_NAME="$PROJECT_NAME.xcworkspace"
+SCHEME="Universal-$PROJECT_NAME"
+
+
+if [ ! hash pod 2>/dev/null ]; then
+  echo "Error: Cocoapods not installed."
+  exit 1  
 fi
 
-# 1
-# Set bash script to exit immediately if any commands fail.
-set -e
-
-# 2
-# Setup some constants for use later on.
-FRAMEWORK_NAME="PrebidMobile"
-LOGDIR=../build/out/log
-mkdir -p "$LOGDIR"
-
-# 3
-# If remnants from a previous build exist, delete them.
-if [ -d "build" ]; then
-rm -rf "build"
+if [ ! -r $PROJECT_DIR/Podfile ] ; then
+  echo "Error: Podfile does not exist."
+  exit 1  
 fi
 
-cd ../sdk/
-LOGFILE="$LOGDIR"/prebid_mobile_build.log
-touch "$LOGFILE"
-
-# 4
-# Build the framework for device and for simulator (using
-# all needed architectures).
-xcodebuild -target "${FRAMEWORK_NAME}" -configuration Release -arch arm64 -arch armv7 -arch armv7s only_active_arch=no defines_module=yes -sdk "iphoneos" > "$LOGFILE" 2>&1 || { echo "Error in build check log "$LOGFILE""; exit;}
-
-xcodebuild -target "${FRAMEWORK_NAME}" -configuration Release -arch x86_64 -arch i386 only_active_arch=no defines_module=yes -sdk "iphonesimulator" > "$LOGFILE" 2>&1 || { echo "Error in build check log "$LOGFILE""; exit;}
-
-# 5
-# Remove .framework file if exists on Desktop from previous run.
-if [ -d "${HOME}/Desktop/${FRAMEWORK_NAME}.framework" ]; then
-rm -rf "${HOME}/Desktop/${FRAMEWORK_NAME}.framework"
+if [ ! hash xcodebuild 2>/dev/null ]; then
+  echo "Error: Xcode or Xcode related command line components not installed."
+  exit 1  
 fi
 
-# 6
-# Copy the device version of framework to Desktop.
-cp -r "build/Release-iphoneos/${FRAMEWORK_NAME}.framework" "${HOME}/Desktop/${FRAMEWORK_NAME}.framework"
-
-# 7
-# Replace the framework executable within the framework with
-# a new version created by merging the device and simulator
-# frameworks' executables with lipo.
-lipo -create -output "${HOME}/Desktop/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "build/Release-iphoneos/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "build/Release-iphonesimulator/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}"
-
-echo "Done! You can find the Prebid Mobile framework on your Desktop"
-echo "Build logs are also available in the build/out/log/ folder."
-
-# 8
-# Delete the most recent build.
-if [ -d "build" ]; then
-rm -rf "build"
+if [ ! -d "$PROJECT_DIR" ]; then
+  echo "Error: $PROJECT_DIR does not exist or is not readable."
+  exit 1
 fi
+
+if [ ! -d "$PROJECT_DIR/$WORKSPACE_NAME" ]; then
+  echo "Error: $PROJECT_DIR/$WORKSPACE_NAME does not exist or is not readable."
+  exit 1
+fi
+
+( cd $PROJECT_DIR; pod install )
+
+( cd $PROJECT_DIR; xcodebuild -workspace $WORKSPACE_NAME -scheme $SCHEME clean archive )
+
+exit 0
